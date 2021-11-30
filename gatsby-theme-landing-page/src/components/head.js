1d7a123880c48f22756b650148f70e26adb36dfc
jxnblk <jxnblk@gmail.com>
2021-11-30 17:02:04 -0500

    Add Head and Layout components

---
+++ gatsby-theme-landing-page/src/components/head.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import { Helmet } from 'react-helmet'
+
+export default function Head ({
+  title,
+  description,
+}) {
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang: 'en-US'
+      }}>
+      <title>{title}</title>
+      <meta name='description' content={description} />
+    </Helmet>
+  )
+}
