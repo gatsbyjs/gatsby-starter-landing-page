975027ae50e0257445c1be18f26d9f7ae2050dea
Max Hawkins <maximuskwame@gmail.com>
2021-12-02 16:34:29 -0800

    structure and style feature component

--- gatsby-theme-landing-page/src/components/features.js
+++ gatsby-theme-landing-page/src/components/features.js
@@ -2,6 +2,7 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import * as React from "react";
 import * as styles from "./features.module.css";
 import MarkdownText from "./markdown-text";
+import Link, { LinkContainer } from "./link";
 
 export default function Features({ heading, secondaryHeading, content }) {
   return (
@@ -9,25 +10,45 @@ export default function Features({ heading, secondaryHeading, content }) {
       <div className={styles.container}>
         <h2 className={styles.heading}>{heading}</h2>
         <h3 className={styles.secondaryHeading}>{secondaryHeading}</h3>
-        <div className={styles.featuresContainer}>
-          {content.map((item) => {
-            <Feature {...item} />;
-          })}
+        <div>
+          {content.map((item, i) => (
+            <Feature
+              {...item}
+              orientation={i % 2 === 0 ? "default" : "reverse"}
+            />
+          ))}
         </div>
       </div>
     </section>
   );
 }
 
-function Feature({ primaryText, secondaryText, image, links }) {
+function Feature({
+  primaryText,
+  secondaryText,
+  image,
+  links,
+  orientation = "default",
+}) {
+  const orientationStyle =
+    orientation === "default" ? "" : styles.featureReverse;
   return (
-    <div className={styles.featureContainer}>
+    <div className={`${styles.featureContainer} ${orientationStyle}`}>
       <div className={styles.copyContainer}>
-        <MarkdownText as="h4" inline {...secondaryText} />
-        <MarkdownText {...primaryText} />
+        <MarkdownText
+          as="h4"
+          className={styles.secondaryText}
+          {...secondaryText}
+        />
+        <MarkdownText className={styles.primaryText} {...primaryText} />
+        <LinkContainer>
+          {links && links.map((link) => <Link key={link.id} {...link} />)}
+        </LinkContainer>
       </div>
       <div className={styles.imageContainer}>
-        <GatsbyImage image={getImage(image)} />
+        <div className={styles.imageWrapper}>
+          <GatsbyImage image={getImage(image)} />
+        </div>
       </div>
     </div>
   );
