49993d0ca32bf69d9fc6d8729177a415755a26cd
Max Hawkins <maximuskwame@gmail.com>
2021-12-03 11:24:17 -0800

    add testimonial scaffold

---
+++ gatsby-theme-landing-page/src/components/testimonial.js
@@ -0,0 +1,6 @@
+import React from "react";
+import * as styles from "./testimonial.module.css";
+
+export default function Testimonial({ heading, secondaryHeading, content }) {
+  return <div></div>;
+}
