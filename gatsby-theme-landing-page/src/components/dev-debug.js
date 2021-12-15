81bff271488a9990d9d05205d849144bbd7a6e5f
jxnblk <jxnblk@gmail.com>
2021-12-15 16:54:00 -0500

    Update dev-debug component is helper message

--- gatsby-theme-landing-page/src/components/dev-debug.js
+++ gatsby-theme-landing-page/src/components/dev-debug.js
@@ -6,5 +6,20 @@ export default function DevDebug(props) {
   if (!IS_DEV) return false;
 
   console.log(props);
-  return <pre children={JSON.stringify(props, null, 2)} />;
+  return (
+    <div
+      style={{
+        maxWidth: 768,
+        margin: "auto",
+        padding: 32,
+        backgroundColor: "#fbb",
+      }}
+    >
+      <h2 style={{ margin: 0, color: "#f30" }}>
+        ⚠️ Could not find the "{props.component}" component.
+      </h2>
+      Ensure that the component exists, is named correctly, and that it's
+      included in: <code>sections/index.js</code>
+    </div>
+  );
 }
